de4588865bfeaee8ed99ccb8e666ce94e03e1f9e
giahienpham <giahien14604@gmail.com>
2024-06-23 15:23:51 +0700

    add 3d model and portfolio

---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,20 @@
+"use client"
+import { Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import clsx from 'clsx'
+import React, { Suspense } from 'react'
+
+const RenderModel = ({children, className}) => {
+  return (
+    <Canvas
+    className={clsx("w-screen h-screen -z-10 relative")}
+    >
+        <Suspense fallback={null}>
+            {children}
+        </Suspense>
+        <Environment preset='city' />
+    </Canvas>
+  )
+}
+
+export default RenderModel
