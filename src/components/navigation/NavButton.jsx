ebf29a34666c3ed02d54809e5cbc1e12c0539b75
giahienpham <giahien14604@gmail.com>
2024-06-28 09:47:41 +0700

    added shadows for the buttons

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
-import {Home} from 'lucide-react'
+import {Github, Home, Linkedin, NotebookText, Palette, Phone, Twitter, User} from 'lucide-react'
 
 const getIcon = (icon) => {
   switch (icon){
     case "home":
       return <Home className='w-full h-auto' strokeWidth={1.5}/>
-      break;
+    case "about":
+      return <User className='w-full h-auto' strokeWidth={1.5}/>
+    case "projects":
+      return <Palette className='w-full h-auto' strokeWidth={1.5}/> 
+    case "contact":
+      return <Phone className='w-full h-auto' strokeWidth={1.5}/>
+    case "github":
+      return <Github className='w-full h-auto' strokeWidth={1.5}/>
+    case "linkedin":
+      return <Linkedin className='w-full h-auto' strokeWidth={1.5}/>
+    case "twitter":
+      return <Twitter className='w-full h-auto' strokeWidth={1.5}/>
+    case "resume":
+      return <NotebookText className='w-full h-auto' strokeWidth={1.5}/>           
     default:
       return <Home className='w-full h-auto' strokeWidth={1.5}/>
   }
@@ -17,7 +30,9 @@ const NavButton = ({x, y, label, link, icon, newTab}) => {
     <div className='absolute cursor-pointer z-50'
     style={{transform: `translate(${x}, ${y})`}}
     >
-      <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center' 
+      <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center
+      bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm
+      ' 
             aria-label={label} name={label}>
               <span className='relative w-14 h-14 p-4'>
                 {getIcon(icon)}
@@ -27,4 +42,4 @@ const NavButton = ({x, y, label, link, icon, newTab}) => {
   )
 }
 
-export default NavButton
+export default NavButton 
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
         foreground: 'rgb(var(--foreground))',
         muted: 'rgb(var(--muted))',
         accent: 'rgb(var(--accent))'
+      },
+      boxShadow:{
+        'glass-inset': 'inset 0 17px 5px -9px rgba(254, 254, 91, 0.05)',
+        'glass-sm': '5px 5px 20px 0px rgba(254, 254, 91, 0.3)',
       }
     },
   },
