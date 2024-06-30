1a5f0038b4a7a015492a3b65f3671e52a694ccfa
giahienpham <giahien14604@gmail.com>
2024-06-30 12:02:52 +0700

    fixed bugs for homebtn

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -8,7 +8,7 @@ import Staff from "@/components/models/Staff";
 export default function Home() {
   return (
     <>
-        <Image src={bg} alt = "background-image" className="fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
+        <Image src={bg} alt = "background-image" className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
         <ProjectList projects = {projectsData}/>
         <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
             <RenderModel>
--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 const HomeBtn = () => {
   return (
     <Link href={"/"} target={'_self'} className='text-foreground rounded-full flex items-center justify-center
-      custom-bg
+      custom-bg fixed top-4 left-4 w-fit self-start
       ' 
             aria-label={"home"} name={"home"}>
-              <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
+              <span className='relative w-14 h-14 p-4 hover:text-accent'>
                 <Home className='w-full h-auto' strokeWidth={1.5}></Home>
               <span className='peer bg-transparent absolute top-0 left-0 w-full h-full'></span>
                 <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2
