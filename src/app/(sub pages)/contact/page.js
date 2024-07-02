5f6ddb579bae44e5ddfad334738fd1ce38e3e410
giahienpham <giahien14604@gmail.com>
2024-07-02 22:57:05 +0700

    add contact form

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,18 +1,25 @@
 import Image from "next/image";
-import bg from "../../../../public/background/about-background.png"
-import RenderModel from "@/components/RenderModel";
-import WizardHat from "@/components/models/WizardHat";
-import AboutDetails from "@/components/about";
+import bg from "../../../../public/background/contact-background.png"
+import Form from "@/components/contact/Form";
 
-export default function Home() {
+export default function Contact() {
   return (
     <>
-        <Image src={bg} alt = "background-image" className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
-        <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0">
-            <RenderModel>
-              <WizardHat/>
-            </RenderModel>
-        </div>
+        <Image 
+          src={bg} 
+          alt = "background-image" 
+          className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
+        <article className="relative w-full flex flex-col items-center justify-center space-y-8"> 
+          <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+            <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+              Summon the wizard
+            </h1>
+            <p className="text-center font-light">
+            Welcome to my realm of digital enchantments! If you are looking to collaborate, explore tech mysteries, or share your ideas, you’re in the right place. Use the form below to send your message through the ethereal network. I will respond with the creativity and expertise that defines my craft. Let’s create something extraordinary together!
+            </p>
+          </div>
+          <Form/>
+        </article>
 
     </>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.variable, "bg-background text-foreground")}>
+      <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>
         {children}
         <FireFliesBackground/>
       </body>
