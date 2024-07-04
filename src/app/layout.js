e074944f28b4c580628113aaae3752d261458d4c
giahienpham <giahien14604@gmail.com>
2024-07-05 00:19:10 +0700

    finalize background sound for the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
         {children}
         <FireFliesBackground/>
         <Sound/>
+        <div id="my-modal"/>
       </body>
     </html>
   );
